59850b6529192ebda36f821b3ba6d1e7672209b1
Brandon Minor <brandon@tangramvision.com>
2021-02-08 12:40:37 -0600

    Start rustdocs for badge crate.

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,46 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 // OF SUCH DAMAGE.
 
+//! Create shields.io-like badges from the comfort and safety of Rust
+//!
+//! RSBadges is a Rust-friendly badges generator. It acts as a library and a command line
+//! interface. Most everything on a badge can be customized:
+//!
+//! - Badge text, for either side
+//! - Badge links, for either side
+//! - Badge color, for either side
+//! - Added logo with the option to embed if desired
+//!
+//!  # Example usage
+//!
+//! ```
+//! use rsbadges::{Badge, Style};
+//! let badge = Badge{
+//!     label_text: String::from("Custom_label"),
+//!     msg_text: String::from("Custom_msg"),
+//!     ..Badge::default()
+//! };
+//! // Create a plastic badge using the data created above.
+//! let badge_style = Style::Plastic(badge);
+//! let badge_svg = badge_style.generate_svg();
+//! rsbadges::save_svg("~/Downloads/badge.svg", badge_svg);
+//! ```
+//!
+
+#![warn(missing_docs)] // warn if there are missing docs
+
 mod badge;
 
 pub use badge::{Badge, BadgeError, Style};
 use std::fs;
 use std::path::Path;
 
+/// A convenience function to save an SVG to a file.
+///
+/// # Errors
+///
+/// This will return [BadgeError::CannotSaveToFile] if the directory
+/// is malformed or cannot be accessed.
 pub fn save_svg(filepath: &str, svg: &str) -> Result<(), BadgeError> {
     let svg_path = Path::new(filepath);
     if let Err(c) = fs::write(svg_path, svg) {
