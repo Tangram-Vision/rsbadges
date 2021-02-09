e9842e634e2ada3e679ae54abe647f860163293a
Brandon Minor <brandon@tangramvision.com>
2021-02-08 22:20:05 -0600

    Add a layout generator for the ForTheBadge type

--- src/badge/generate_layout.rs
+++ src/badge/generate_layout.rs
@@ -171,3 +171,74 @@ pub(crate) fn flat_or_square(badge: &Badge) -> Result<Layout, BadgeError> {
 
     Ok(layout)
 }
+
+pub(crate) fn for_the_badge(badge: &Badge) -> Result<Layout, BadgeError> {
+    let mut layout = Layout::default();
+
+    // Normalize text
+    let font = load_regular_font()?;
+    let (label_text_norm, label_text_width) =
+        get_text_dims(&font, &badge.label_text.to_uppercase(), 10.0, 1.15);
+    layout.label_text_norm = label_text_norm;
+    layout.label_text_width = label_text_width;
+    let bold_font = load_bold_font()?;
+    let (msg_text_norm, msg_text_width) =
+        get_text_dims(&bold_font, &badge.msg_text.to_uppercase(), 10.0, 0.5);
+    layout.msg_text_norm = msg_text_norm;
+    layout.msg_text_width = msg_text_width;
+
+    // Padding and spacing calculations
+    let horiz_padding = 9.0;
+    layout.badge_height = 28.0;
+
+    // Logo padding and width
+    let mut total_logo_width = 0.0;
+    layout.logo_width = 0.0;
+    layout.logo_padding = 0.0;
+    if !badge.logo.is_empty() {
+        if !badge.label_text.is_empty() {
+            layout.logo_padding = 3.0;
+        }
+        let logo_height = 14.0;
+        layout.logo_y = (layout.badge_height - logo_height) * 0.5;
+        layout.logo_x = horiz_padding;
+        layout.logo_width = 14.0;
+        total_logo_width = layout.logo_width + layout.logo_padding;
+    }
+
+    // Label padding and width
+    // This logic is something else, lemme tell ya
+    layout.label_total_width = layout.label_text_width + total_logo_width + 10.0;
+    if !badge.label_text.is_empty() {
+        layout.label_total_width += 10.0 + (2.0 * layout.label_text_norm.len() as f32)
+    } else if !badge.logo.is_empty() {
+        if !badge.label_text.is_empty() {
+            layout.label_total_width += 7.0;
+        } else {
+            layout.label_total_width -= 7.0;
+        }
+    } else {
+        layout.label_total_width -= 11.0;
+    }
+
+    layout.label_text_x = (layout.label_total_width + total_logo_width) / 2.0;
+    layout.label_text_width = layout.label_total_width - (24.0 + total_logo_width);
+
+    // Message padding and width
+    layout.msg_total_width =
+        layout.msg_text_width + 20.0 + (1.5 * layout.label_text_norm.len() as f32);
+    layout.msg_text_x = layout.label_total_width + (layout.msg_total_width / 2.0);
+    layout.msg_text_width = layout.msg_total_width - 24.0;
+
+    // Scale back up for the SVG
+    layout.label_text_width *= 10.0;
+    layout.msg_text_width *= 10.0;
+    layout.label_text_x *= 10.0;
+    layout.msg_text_x *= 10.0;
+
+    // Color conversion to string
+    layout.label_color = verify_color(&badge.label_color)?;
+    layout.msg_color = verify_color(&badge.msg_color)?;
+
+    Ok(layout)
+}
